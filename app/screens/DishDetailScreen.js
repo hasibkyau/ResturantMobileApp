25563f78d9f101c1d4be52c6fc265cca15d523bd
hasibkyau <hasibkyau.cse@gmail.com>
2022-03-29 06:54:00 +0600

    Setting up project

---
+++ app/screens/DishDetailScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+const DishDetailScreen = () =>{
+    return(
+        <View><Text>DishDetailScreen</Text></View>
+    )
+}
+
+export default DishDetailScreen;
